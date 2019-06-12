e6bf48776994bae9840754e944e7f0a518cfebfb
Robert Askam <Robert.Askam@glasscare.local>
2019-06-12 13:46:24 +0100

    New Status

--- index.php
+++ index.php
@@ -49,12 +49,6 @@ $bitcoin->getinfo();
 $bitcoin->getrawtransaction('e87f138c9ebf5986151667719825c28458a28cc66f69fed4f1032a93b399fdf8', 1);
 $bitcoin->getblock('00000000000000000018a65ff0bbbc2a93493c693d05dd65c6a8dcbb881f55fb');
 
-
-// echo $bitcoin->error;
-
-echo "<br>";
-
-echo $bitcoin->status;
 ?>
 
 <!DOCTYPE html>
