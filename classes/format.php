def94170121b1233565f00f2221621ee25b50301
Robert Askam <Robert.Askam@glasscare.local>
2019-06-12 15:43:21 +0100

    Fix

--- classes/format.php
+++ classes/format.php
@@ -35,6 +35,11 @@ Class PHPFormat
         $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
         $pow = min($pow, count($units) - 1); 
         
+
+        // Uncomment one of the following alternatives
+        // $bytes /= pow(1024, $pow);
+        $bytes /= (1 << (10 * $pow)); 
+
         return round($bytes, $precision) . ' ' . $units[$pow]; 
     } 
        
