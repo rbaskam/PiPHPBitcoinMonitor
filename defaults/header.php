b139fd3bb25b263f8d1d9f5035a2d1a7e93826d0
Robert Askam <Robert.Askam@glasscare.local>
2019-06-12 14:57:24 +0100

    TEst

--- defaults/header.php
+++ defaults/header.php
@@ -5,15 +5,22 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('max_execution_time', 0);
 
-//Set the Bitcoin Login Details
-$userName = getenv('BTCNODEUSER');
-$password = getenv('BTCNODEPASS');
+
 
 //Require the composer packages
 require dirname(__DIR__, 1) .'/vendor/autoload.php';
 
 //Require the BTC Class
 require dirname(__DIR__, 1) .'/classes/bitcoin.php';
+
+
+//Get ENV data
+$dotenv = Dotenv\Dotenv::create(dirname(__DIR__, 1));
+$dotenv->load();
+
+//Set the Bitcoin Login Details
+$userName = getenv('BTCNODEUSER');
+$password = getenv('BTCNODEPASS');
 
 //Connect tot he RPC Client
 $url = 'http://' . $userName . ':' . $password . '@localhost:8332/';
@@ -23,8 +30,3 @@ echo $url;
 echo "<br>";
 
 var_dump($bitcoinRPC);
-
-//Get ENV data
-$dotenv = Dotenv\Dotenv::create(dirname(__DIR__, 1));
-$dotenv->load();
-
